d22dbc0beb44295a52c3635703f2c2ad8253b52b
PriyamVR <149721275+PriyamVR@users.noreply.github.com>
2026-09-15 04:55:12 +0600

    Add CategorySales MapReduce implementation

--- mapreduce/CategorySales.java
+++ mapreduce/CategorySales.java
@@ -1,2 +1,93 @@
 // Category-wise Total Sales Analysis
 // Hadoop MapReduce Implementation
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class CategorySales {
+
+    public static class SalesMapper 
+        extends Mapper<Object, Text, Text, DoubleWritable> {
+
+        private Text category = new Text();
+        private DoubleWritable sales = new DoubleWritable();
+
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+
+            String[] fields = value.toString().split(",");
+
+            try {
+                category.set(fields[2]);
+                sales.set(Double.parseDouble(fields[6]));
+
+                context.write(category, sales);
+
+            } catch(Exception e) {
+                // Ignore invalid rows
+            }
+        }
+    }
+
+
+    public static class SalesReducer 
+        extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+
+        private DoubleWritable result = new DoubleWritable();
+
+        public void reduce(Text key, Iterable<DoubleWritable> values,
+                Context context)
+                throws IOException, InterruptedException {
+
+            double total = 0;
+
+            for(DoubleWritable val : values) {
+                total += val.get();
+            }
+
+            result.set(total);
+
+            context.write(key, result);
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf,
+                "Category Wise Total Sales Analysis");
+
+
+        job.setJarByClass(CategorySales.class);
+
+
+        job.setMapperClass(SalesMapper.class);
+        job.setReducerClass(SalesReducer.class);
+
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+
+
+        FileInputFormat.addInputPath(job,
+                new Path(args[0]));
+
+        FileOutputFormat.setOutputPath(job,
+                new Path(args[1]));
+
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
